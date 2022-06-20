fc5e3a63c0c71ffdbbf8bc8abbbd6ef7c661dd70
Snowy <34697265+SnowyGFL@users.noreply.github.com>
2022-06-20 23:47:56 +0800

    Initial commit

---
+++ MusicNameParser/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+string currentPath = AppContext.BaseDirectory;
+string outputPath = Directory.CreateDirectory("output").FullName;
+foreach (var file in GetAllFiles(currentPath))
+{
+    Console.WriteLine($"Parsing: {Path.GetFileName(file)}");
+    string fileName = Path.GetFileNameWithoutExtension(file);
+    string[] lines = File.ReadAllLines(file).ToArray();
+    StringBuilder sb = new();
+    sb.AppendLine(@"""music""");
+    sb.AppendLine("{");
+    foreach (string line in lines)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+            continue;
+
+        string[] split = line.Split("||");
+        if (split.Length > 1)
+        {
+            string musicname = Path.GetFileName(split[0]);
+            sb.AppendLine($"\t\"{musicname}\"\t\t\t\"{split[1]}\"");
+        }
+    }
+
+    sb.AppendLine("}");
+
+    File.WriteAllText(Path.Combine(outputPath, $"{fileName}.cfg"), sb.ToString());
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("The end. Press a key to exit...");
+Console.ReadKey();
+
+static string[] GetAllFiles(string currentPath) => Directory.GetFiles($@"{currentPath}", "*txt").ToArray();
---
+++ README.md
@@ -0,0 +1,3 @@
+## Music config parser
+
+This is a simple program that parses [Zeddy's](https://github.com/notkoen/zeddy-server-public-configs/tree/main/Song%20Name%20Configs) song name configs to GFL's version.
